cf134900fb802f4caee99ae3bf15ba4ba0d98151
Masuo-Naoki <naoki.masuo@rizapgroup.com>
2025-01-22 16:51:04 +0900

    Railsの環境構築

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  validates :name, presence: { message: 'を入力してください。' }
+  validates :ruby, presence: { message: 'を入力してください。' }, format: {with: /\A[ァ-ヴー]+\s[ァ-ヴー]+\z/u, message: "はカタカナのみ入力してください。"}
+
 end
+
