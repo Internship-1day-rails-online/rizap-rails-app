667ba05c0930d1ddecf0869cf68a1c8c7a7b57a6
fakanuma <fakanuma@binghamton.edu>
2025-01-24 03:16:08 -0500

    showアクションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
         @users = User.all #allはすべてのデータを取得するメソッド
     end
 
+    def show
+        @user = User.find(params[:id]) #findは指定したidのデータを取得するメソッド
+    end
+    
 end
